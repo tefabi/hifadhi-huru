6d4dbfd0085aa19d023658e303255c6954f33ca5
Timothy Karani <kartimothy@gmail.com>
2022-12-03 23:54:18 +0300

    First of many

---
+++ src/main/java/com/tefabi/hifadhi/huru/HifadhiHuruApplication.java
@@ -0,0 +1,13 @@
+package com.tefabi.hifadhi.huru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HifadhiHuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HifadhiHuruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tefabi/hifadhi/huru/HifadhiHuruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tefabi.hifadhi.huru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HifadhiHuruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
